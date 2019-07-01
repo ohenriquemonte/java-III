8801af0fd1e16640467002e42e0b699d6968e670
montenegrohenrique <montenegrohenrique@192.168.1.104>
2019-06-30 23:26:44 -0300

    Aula 07 - Praticando heranca e interfaces

--- src/ClienteSistema.java
+++ src/ClienteSistema.java
@@ -1,19 +1,19 @@
 
 public class ClienteSistema implements Autenticavel {
 
-	private int senha;
+	private AutenticacaoUtil autenticador;
+	
+	public ClienteSistema() {
+		 this.autenticador = new AutenticacaoUtil();
+	}
 	
 	@Override
 	public void setSenha(int senha) {
-		this.senha = senha;
+		this.autenticador.setSenha(senha);
 	}
 	
 	@Override
 	public boolean autentica(int senha) {
-		if(this.senha == senha) {
-			return true;
-		} else {
-			return false;
-		}
+		return this.autenticador.autentica(senha);
 	}
 }
--- src/Gerente.java
+++ src/Gerente.java
@@ -1,25 +1,24 @@
 
 public class Gerente extends Funcionario implements Autenticavel {
 	
-	private int senha;
+	private AutenticacaoUtil autenticador;
+	
+	public Gerente() {
+		 this.autenticador = new AutenticacaoUtil();
+	}
+	
+	@Override
+	public void setSenha(int senha) {
+		this.autenticador.setSenha(senha);
+	}
+	
+	@Override
+	public boolean autentica(int senha) {
+		return this.autenticador.autentica(senha);
+	}
 	
 	public double getBonificacao() {
 		System.out.println("metodo de bonificacao do gerente");
 		return super.getSalario();
-	}
-
-	@Override
-	public void setSenha(int senha) {
-		this.senha = senha;
-		
-	}
-
-	@Override
-	public boolean autentica(int senha) {
-		if(this.senha == senha) {
-			return true;
-		} else {
-			return false;
-		}
 	}
 }
